d81a74a97a9e514a6b497482e6c41415ed14f02a
Tom Kaufman <tommyk1995@gmail.com>
2023-06-06 21:15:50 -0400

    Add ::File enum to ApiResponse

--- cyprus-server/src/server/api_response.rs
+++ cyprus-server/src/server/api_response.rs
@@ -1,9 +1,18 @@
-use axum::{http, response};
+use axum::{
+    body::StreamBody,
+    http::{
+        self,
+        header::{self, HeaderMap},
+    },
+    response,
+};
 use serde::Serialize;
+use tokio_util::io::ReaderStream;
 
 pub enum ApiResponse<T: Serialize> {
     Success(T),
     Error(http::StatusCode, String),
+    File(tokio::fs::File, String),  // file object, file name
 }
 
 #[derive(Serialize)]
@@ -31,6 +40,24 @@ where
                     message,
                 };
                 (status_code, response::Json::from(resp.message)).into_response()
+            }
+            ApiResponse::File(file, filename) => {
+                let stream = ReaderStream::new(file);
+                let body = StreamBody::new(stream);
+
+                let mut headers = HeaderMap::new();
+                headers.insert(header::CONTENT_TYPE, "audio/mp4".parse().unwrap());
+                headers.insert(
+                    header::CONTENT_DISPOSITION,
+                    format!(
+                        "attachment; filename=\"{}\"",
+                        filename
+                    )
+                    .parse()
+                    .unwrap(),
+                );
+
+                (headers, body).into_response()
             }
         }
     }
